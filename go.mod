7fb5cfae16f8a8ffde26c7cc5c7dcf36b2debbb0
Hassan Sani <saniyhassan@gmail.com>
2021-05-27 10:13:35 +0100

    added enviroment

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.16
 
 require (
 	github.com/gofiber/fiber/v2 v2.10.0
+	github.com/joho/godotenv v1.3.0 // indirect
 	github.com/kamva/mgm/v3 v3.1.0
 	go.mongodb.org/mongo-driver v1.5.2
 )
--- main.go
+++ main.go
@@ -1,29 +1,23 @@
 package main
 
 import (
-	"context"
-	"time"
-
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/inidaname/hotelapi/routes"
-	"github.com/kamva/mgm/v3"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
+	"github.com/joho/godotenv"
 )
-
-func init() {
-	// Setup the mgm default config
-	err := mgm.SetDefaultConfig(nil, "mgm_lab", options.Client().ApplyURI("mongodb://root:12345@localhost:27017"))
-}
 
 func main() {
 	app := fiber.New()
 
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		panic(err)
+	}
+
 	app.Use(cors.New())
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+
 	app.Use(func(c *fiber.Ctx) error {
 		if c.Is("json") {
 			return c.Next()
